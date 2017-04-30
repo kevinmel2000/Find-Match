64e2666f571ebd0ab8b24066e5ffeebca49fad29
omrobbie <putu.roby@yahoo.com>
2017-04-30 09:11:59 +0800

    mobil dimunculkan random dari 1 object

---
+++ Assets/Scripts/ControlSpawn.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ControlSpawn : MonoBehaviour
+{
+    float waitShow = 0;
+    float timer = 0;
+
+    public GameObject spawn;
+    public GameObject[] ob;
+
+    private void Start()
+    {
+        waitShow = Random.Range(0f, 3f);
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > waitShow)
+        {
+            GameObject.Instantiate(ob[Random.Range(0, (ob.Length))], spawn.transform.position, spawn.transform.rotation);
+            waitShow = Random.Range(4f, 6f);
+            timer = 0;
+        }
+    }
+}
