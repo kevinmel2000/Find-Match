42e144807cec2e40e8d234edbbf8bd82c2161300
omrobbie <putu.roby@yahoo.com>
2017-04-30 09:48:27 +0800

    spawn 3 mobil random dari object per update

--- Assets/Scripts/ControlSpawn.cs
+++ Assets/Scripts/ControlSpawn.cs
@@ -2,26 +2,55 @@
 
 public class ControlSpawn : MonoBehaviour
 {
-    float waitShow = 0;
-    float timer = 0;
+    float waitShow1 = 0;
+    float waitShow2 = 0;
+    float waitShow3 = 0;
 
-    public GameObject spawn;
+    float timer1 = 0;
+    float timer2 = 0;
+    float timer3 = 0;
+
+    public GameObject spawn1;
+    public GameObject spawn2;
+    public GameObject spawn3;
+
     public GameObject[] ob;
+    float waitTime = 0;
 
     private void Start()
     {
-        waitShow = Random.Range(0f, 3f);
+        waitShow1 = Random.Range(3f, 5f);
+        waitShow2 = Random.Range(1f, 4f);
+        waitShow3 = Random.Range(0.5f, 5f);
+
+        waitTime = Random.Range(6f, 10f);
     }
 
     private void Update()
     {
-        timer += Time.deltaTime;
+        timer1 += Time.deltaTime;
+        timer2 += Time.deltaTime;
+        timer3 += Time.deltaTime;
 
-        if(timer > waitShow)
+        if (timer1 > waitShow1)
         {
-            GameObject.Instantiate(ob[Random.Range(0, (ob.Length))], spawn.transform.position, spawn.transform.rotation);
-            waitShow = Random.Range(4f, 6f);
-            timer = 0;
+            GameObject.Instantiate(ob[Random.Range(0, (ob.Length))], spawn1.transform.position, spawn1.transform.rotation);
+            waitShow1 = waitTime;
+            timer1 = 0;
+        }
+
+        if (timer2 > waitShow2)
+        {
+            GameObject.Instantiate(ob[Random.Range(0, (ob.Length))], spawn2.transform.position, spawn2.transform.rotation);
+            waitShow2 = waitTime;
+            timer2 = 0;
+        }
+
+        if (timer3 > waitShow3)
+        {
+            GameObject.Instantiate(ob[Random.Range(0, (ob.Length))], spawn3.transform.position, spawn3.transform.rotation);
+            waitShow3 = waitTime;
+            timer3 = 0;
         }
     }
 }
