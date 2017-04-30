b256556ddab3ecbc9f9249b3590057dcfbb1ac64
omrobbie <putu.roby@yahoo.com>
2017-04-30 08:47:33 +0800

    object mobil sudah bisa bergerak

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    float range = 0f;
+    float count = 0f;
+    float speed = 0.6f;
+    public Sprite[] img;
+
+    private void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = img[Random.Range(0, img.Length)];
+        range = Random.Range(40f, 300f);
+    }
+
+    private void Update()
+    {
+        transform.Translate(speed * Time.deltaTime * 1f, speed * Time.deltaTime * move_steer(), 0);
+        if(transform.position.x > 8f)
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+    float move_steer()
+    {
+        float value = 0f;
+        count += 1f;
+
+        if (count <= range) value = 1f;
+        if (count > range) value = -1f;
+        if (count > (range * 2) - 1) count = 0f;
+        return value;
+    }
+}
